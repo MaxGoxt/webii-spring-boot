03f0029745ed46c79eac75dd5935c05eecb4e8b0
MaxGoxt <m.gomezcoitino@gmail.com>
2025-09-22 17:36:43 -0300

    first commit

---
+++ src/test/java/com/ifsul/WebII/WebIiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ifsul.WebII;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebIiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
